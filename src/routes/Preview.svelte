58037f571562309cd1204fbd2338c35a57423942
kauderk <65237382+kauderk@users.noreply.github.com>
2023-02-14 02:21:18 -0500

    preview

---
+++ src/routes/Preview.svelte
@@ -0,0 +1,257 @@
+<div
+	aria-label="YouTube Video Player"
+	data-version="/s/player/97ea7458/player_ias.vflset/en_US/base.js"
+	id="movie_player"
+	tabindex="-1"
+	class="html5-video-player ytp-fine-scrubbing-exp">
+	<div aria-live="polite" data-layer="4" class="ytp-preview ytp-tooltip">
+		<div class="ytp-tooltip-text-wrapper" aria-hidden="true">
+			<div
+				class="ytp-tooltip-edu tether-abutted tether-abutted-top tether-element-attached-top tether-element-attached-center tether-target-attached-top tether-target-attached-center "
+				style="bottom: 92px;">
+				<svg
+					height="100%"
+					viewBox="0 0 36 36"
+					width="100%"
+					class="tether-target tether-enabled tether-abutted tether-abutted-top tether-element-attached-top tether-element-attached-center tether-target-attached-top tether-target-attached-center "
+					><path
+						d="M14.1 36.75 12 34.65 24 22.65 36 34.65 33.9 36.75 24 26.85ZM14.1 24.1 12 22 24 10 36 22 33.9 24.1 24 14.2Z"
+						class="" /></svg>
+				<span class="">Pull up for precise seeking</span>
+			</div>
+			<div class="ytp-tooltip-image" />
+			<div
+				class="ytp-tooltip-title "
+				style="right: 81px; text-align: center;">
+				<svg height="100%" viewBox="0 0 24 24" width="100%" class=""
+					><g class=""
+						><rect
+							fill="none"
+							height="24"
+							width="24"
+							x="0"
+							class="" /></g
+					><g class=""
+						><g class=""
+							><polygon
+								points="19,9 20.25,6.25 23,5 20.25,3.75 19,1 17.75,3.75 15,5 17.75,6.25"
+								class="" /><polygon
+								points="19,15 17.75,17.75 15,19 17.75,20.25 19,23 20.25,20.25 23,19 20.25,17.75"
+								class="" /><path
+								d="M11.5,9.5L9,4L6.5,9.5L1,12l5.5,2.5L9,20l2.5-5.5L17,12L11.5,9.5z M9.99,12.99L9,15.17l-0.99-2.18L5.83,12l2.18-0.99 L9,8.83l0.99,2.18L12.17,12L9.99,12.99z"
+								class="" /></g
+						></g
+					></svg> <span class="">Intro</span>
+			</div>
+			<span class="ytp-tooltip-text ">0:20</span>
+		</div>
+		<div
+			class="ytp-tooltip-bg "
+			style="width: 158px; height: 90px; background: url(&quot;https://i.ytimg.com/sb/ZwLekxsSY3Y/storyboard3_L2/M0.jpg?sqp=-oaymwENSDfyq4qpAwVwAcABBqLzl_8DBgik8IiWBg==&amp;sigh=rs%24AOn4CLCA_UOBs_atnULKzTgWlnQqwVRvhA&quot;) -160px -360px / 800px 450px;">
+			<div class="ytp-tooltip-duration ">12:27</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.html5-video-player {
+		position: relative;
+		width: 100%;
+		height: 100%;
+		/* overflow: hidden;  */
+		z-index: 0;
+		outline: 0;
+		font-family: 'YouTube Noto', Roboto, Arial, Helvetica, sans-serif;
+		color: #eee;
+		text-align: left;
+		direction: ltr;
+		font-size: 11px;
+		line-height: 1.3;
+		-webkit-font-smoothing: antialiased;
+		-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+		-ms-touch-action: manipulation;
+		touch-action: manipulation;
+		-ms-high-contrast-adjust: none;
+	}
+
+	.html5-video-player:not(.ytp-transparent) {
+		background-color: #000;
+	}
+
+	.ytp-tooltip {
+		position: absolute;
+		z-index: 1002;
+		font-size: 118%;
+		font-weight: 500;
+		line-height: 15px;
+		opacity: 0;
+		-webkit-transition: -webkit-transform 0.1s cubic-bezier(0, 0, 0.2, 1),
+			opacity 0.1s cubic-bezier(0, 0, 0.2, 1);
+		transition: transform 0.1s cubic-bezier(0, 0, 0.2, 1),
+			opacity 0.1s cubic-bezier(0, 0, 0.2, 1),
+			-webkit-transform 0.1s cubic-bezier(0, 0, 0.2, 1),
+			opacity 0.1s cubic-bezier(0, 0, 0.2, 1);
+		pointer-events: none;
+	}
+
+	.ytp-tooltip:not([aria-hidden='true']) {
+		opacity: 1;
+		-webkit-transform: none;
+		transform: none;
+		-webkit-transition: -webkit-transform 0.1s cubic-bezier(0.4, 0, 1, 1),
+			opacity 0.1s cubic-bezier(0.4, 0, 1, 1);
+		transition: transform 0.1s cubic-bezier(0.4, 0, 1, 1),
+			opacity 0.1s cubic-bezier(0.4, 0, 1, 1),
+			-webkit-transform 0.1s cubic-bezier(0.4, 0, 1, 1),
+			opacity 0.1s cubic-bezier(0.4, 0, 1, 1);
+	}
+
+	.ytp-tooltip.ytp-preview {
+		padding: 2px;
+		background-color: rgba(28, 28, 28, 0.9);
+		border-radius: 2px;
+	}
+
+	.ytp-tooltip.ytp-preview:not(.ytp-text-detail) {
+		background-color: #fff;
+	}
+
+	.html5-video-player:not(.ytp-touch-mode) :-webkit-scrollbar {
+		width: 10px;
+		background-color: #424242;
+	}
+
+	.html5-video-player:not(.ytp-touch-mode) :-webkit-scrollbar-thumb {
+		background-color: #8e8e8e;
+		border: 1px solid #424242;
+		border-radius: 5px;
+	}
+
+	.html5-video-player:not(.ytp-touch-mode) :-webkit-scrollbar-track {
+		background-color: #424242;
+	}
+
+	.ytp-tooltip-text-wrapper {
+		border-radius: 3px;
+		float: right;
+	}
+
+	.ytp-tooltip.ytp-preview:not(.ytp-text-detail) .ytp-tooltip-text-wrapper {
+		position: absolute;
+		bottom: 5px;
+		left: 0;
+		width: 100%;
+		text-align: center;
+		z-index: 1;
+	}
+
+	.ytp-tooltip-bg {
+		background-color: #000;
+		background-repeat: no-repeat;
+		position: relative;
+		display: none;
+	}
+
+	.ytp-tooltip.ytp-preview .ytp-tooltip-bg {
+		display: block;
+	}
+
+	.ytp-tooltip-edu {
+		display: none;
+	}
+
+	.ytp-fine-scrubbing-exp
+		.ytp-preview:not(.ytp-text-detail)
+		.ytp-tooltip-edu {
+		display: block;
+		text-align: left;
+		width: 103%;
+		height: auto;
+		font-size: 12px;
+		position: absolute;
+		text-shadow: 0 0 4px rgba(0, 0, 0, 0.75);
+	}
+
+	.ytp-tooltip-image {
+		width: 20px;
+		height: 20px;
+		border: 0;
+		border-radius: 10px;
+		position: absolute;
+		display: none;
+		top: 2px;
+		left: 7px;
+		background-size: contain;
+	}
+
+	.ytp-tooltip-title {
+		display: none;
+	}
+
+	.ytp-preview:not(.ytp-text-detail) .ytp-tooltip-title {
+		width: 200%;
+		text-align: center;
+		right: 80px;
+		display: block;
+		overflow: hidden;
+		position: relative;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+		height: 16px;
+		background-color: none;
+		border: 0;
+		top: 45px;
+		text-shadow: 0 0 4px rgba(0, 0, 0, 0.75);
+	}
+
+	span {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		background: transparent;
+	}
+
+	.ytp-tooltip-text {
+		display: block;
+		background-color: rgba(28, 28, 28, 0.9);
+		border-radius: 2px;
+		padding: 5px 9px;
+	}
+
+	.ytp-tooltip.ytp-preview .ytp-tooltip-text {
+		display: inline;
+		border-bottom-left-radius: 0;
+		border-bottom-right-radius: 0;
+	}
+
+	.ytp-preview:not(.ytp-text-detail) .ytp-tooltip-text {
+		background: none;
+		position: relative;
+		top: 45px;
+		text-shadow: 0 0 4px rgba(0, 0, 0, 0.75);
+	}
+
+	.ytp-tooltip-duration {
+		display: none;
+	}
+
+	.html5-video-player svg {
+		pointer-events: none;
+	}
+
+	.ytp-fine-scrubbing-exp
+		.ytp-preview:not(.ytp-text-detail)
+		.ytp-tooltip-edu
+		svg {
+		display: inline-block;
+		width: 12px;
+		height: 12px;
+		margin-right: 3px;
+		fill: white;
+		text-shadow: 0 0 4px rgba(0, 0, 0, 0.75);
+	}
+
+	.ytp-tooltip-title svg {
+		display: none;
+	}
+</style>
