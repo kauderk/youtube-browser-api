60bc57d397feb109ba060806649f7a8a27c9abcf
kauderk <65237382+kauderk@users.noreply.github.com>
2023-02-15 12:40:50 -0500

    data page

--- src/routes/api/youtube/data/+page.svelte
+++ src/routes/api/youtube/data/+page.svelte
@@ -1,70 +1,95 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
-	import Api from '../../../../api'
-	const config = <const>{
-		query: {
-			id: '3sWTzMsmdx8',
-			sugestions: true,
-			contentPage: true,
-			title: true,
-			isLive: true,
-			channel: true,
-			description: true,
-			chapters: true,
-			auto_chapters: true,
-			heatmap: true,
-			heatmapPath: true,
-		},
-	}
-	export let data
+	import Legend from '../components/Legend.svelte'
+	import Item from '../components/Item.svelte'
+	import type { Params } from './+server'
+	import type { NonNullableNested } from '../utility-types'
+	import { toProps, type Pre } from './utils'
+	import { common } from '../components/common'
 
-	onMount(() => console.log(data))
+	//#region Types
+	type Model = NonNullableNested<Params>
+	type Multiple<M = Filter<Model>['objects']> = {
+		[K in keyof M]?: {
+			[key in keyof M[K]]: Pre<M[K][key]>
+		}
+	}
+	type Single<M = Filter<Model>['symbols']> = {
+		[K in keyof M]?: Pre<M[K]>
+	}
+	type Filter<T> = {
+		symbols: Pick<
+			T,
+			{ [K in keyof T]: T[K] extends string ? K : never }[keyof T]
+		>
+		objects: Pick<
+			T,
+			{ [K in keyof T]: T[K] extends object ? K : never }[keyof T]
+		>
+	}
+	export const ObjectKeys = <obj extends o>(o: obj) =>
+		Object.keys(o) as (keyof typeof o)[]
+	//#endregion
+
+	const Single = {
+		channel: {
+			intent: 'necessary',
+			shim: '/',
+			placeholder: 'ColdFusion',
+		},
+		suggestion: {
+			param: 0,
+		},
+	} satisfies Single<Model>
+	let Multiple = {
+		playlist: {
+			playlistId: common.playlistId[1],
+			limit: {
+				param: 10,
+			},
+		},
+		search: {
+			keyword: {
+				placeholder: 'How to...',
+				intent: 'necessary',
+				shim: 'fa-search',
+			},
+			withPlaylist: { param: false },
+			limit: { param: 10 },
+			option: {
+				placeholder: `'' | 'video' | 'channel' | 'playlist' | 'movie'`,
+				shim: 'fa-photo-film-music',
+			},
+		},
+	} satisfies Multiple<Model>
+
+	$: log = Object.values(Multiple).reduce(
+		(acc, sub) =>
+			(acc += Object.values(sub).reduce(
+				(a, b) => (a += b.param || ''),
+				''
+			)),
+		''
+	)
+	$: console.log({ Multiple, Single })
 </script>
 
 <section class="space-y-4">
-	<p class="text-center">
+	<p class="text-center card p-4 card-header">
 		<strong>Data Endpoint</strong>
 	</p>
-
-	<div class="card p-4 grid grid-cols-1 md:grid-cols-2 gap-4">
-		<label class="label">
-			<span>Playlist</span>
-			<div
-				class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-				<div class="input-group-shim">?list=</div>
-				<input
-					type="text"
-					placeholder="PLh0j6fxciWbLVMwgAGx6UprZZQGvD-cYG" />
-			</div>
-		</label>
-		<!-- --- -->
-		<label class="label">
-			<span>Channel</span>
-			<div
-				class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-				<div class="input-group-shim">/</div>
-				<input type="text" placeholder="ColdFusion" />
-			</div>
-		</label>
-		<!-- --- -->
-		<label class="label">
-			<span>Suggestion</span>
-			<div
-				class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-				<input type="text" placeholder="How to..." />
-			</div>
-		</label>
-		<!-- --- -->
-		<label class="label">
-			<span>Search</span>
-			<div
-				class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-				<div class="input-group-shim">
-					<i class="fa-solid fa-search" />
-				</div>
-				<input type="search" placeholder="YMH" />
-				<button class="variant-filled-secondary">Submit</button>
-			</div>
-		</label>
-	</div>
+	{log}
+	{#each ObjectKeys(Multiple) as title}
+		<Legend {title} />
+		<div class="card p-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+			{#each Object.entries(Multiple[title]).map(toProps) as props}
+				<Item {...props} />
+			{/each}
+		</div>
+	{/each}
+	{#each Object.entries(Single) as entry}
+		<Legend title={entry[0]} />
+		<div class="card p-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+			<Item {...toProps(entry)} />
+		</div>
+	{/each}
 </section>
