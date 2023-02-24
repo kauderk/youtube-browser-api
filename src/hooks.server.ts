603c60055ac4106da763f752beb11c47b47ea8c8
kauderk <65237382+kauderk@users.noreply.github.com>
2023-02-24 18:04:25 -0500

    package & sveltekit-zero-api preflight requests

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,24 @@
 import type { Handle } from '@sveltejs/kit'
 
+const allowAnyOrigin = { 'Access-Control-Allow-Origin': '*' }
 export const handle: Handle = async ({ event, resolve }) => {
-	if (event.request.method === 'GET') {
-		event.setHeaders({
-			'Access-Control-Allow-Origin': '*',
+	if (event.request.method === 'OPTIONS') {
+		// Respond to preflight requests
+		// I'm assuming that sveltekit internal fetch()/Requests
+		// comply with these headers
+		// but to allow sveltekit-zero-api's handler function to do it's job it must be explicit
+		return new Response(null, {
+			headers: {
+				...allowAnyOrigin,
+				'Access-Control-Allow-Methods': 'GET',
+				'Access-Control-Allow-Headers':
+					'Content-Type, cache-control, x-requested-with',
+			},
 		})
+	}
+
+	if (event.request.method === 'GET') {
+		event.setHeaders(allowAnyOrigin)
 	}
 
 	return await resolve(event)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-import Api from './api'
+import { createZeroApi } from 'sveltekit-zero-api/api'
+import type { GeneratedAPI } from './sveltekit-zero-api'
 
-export default Api
+const routes = createZeroApi({
+	baseUrl: 'https://youtube-browser-api.netlify.app',
+	onError: async err => console.error('[youtube-browser-api]', err),
+	prependCallbacks(method) {
+		method.Error(err => {
+			console.warn('Error', err)
+		})
+	},
+}) as GeneratedAPI
+
+export default routes
