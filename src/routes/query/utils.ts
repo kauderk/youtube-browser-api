b97467eab8f0d25afd6a066e72feb077d1fd285b
kauderk <65237382+kauderk@users.noreply.github.com>
2023-04-19 12:43:47 -0500

    leverage null coalesce on Path & PathValue types

--- src/routes/query/utils.ts
+++ src/routes/query/utils.ts
@@ -38,15 +38,28 @@ export type DeepPartial<T> = OptionalStringOr<
 		>
 	>
 >
+type N = null | undefined
+type Clear<T> = NonNullable<Exclude<T, N>>
+type JSON = Record<string | number, any>
+
+// prettier-ignore
 
 // https://twitter.com/diegohaz/status/1309489079378219009/photo/1
 type PathImpl<T, K extends keyof T> = K extends string | number
-	? T[K] extends Record<string | number, any>
-		? T[K] extends ArrayLike<any>
-			? K | `${K}.${PathImpl<T[K], number>}`
-			: K | `${K}.${PathImpl<T[K], keyof T[K]>}`
-		: K
-	: never
+    ? T[K] extends { end: true }
+        ?  K// early return if T[K] extends { end: true }
+        : T[K] extends JSON
+            // clean
+            ? T[K] extends ArrayLike<any>
+                ? `${K}.${PathImpl<T[K], number>}`
+                : K | `${K}.${PathImpl<T[K], keyof T[K]>}`
+            // null coalesce
+            : T[K] extends JSON | N
+                ? T[K] extends ArrayLike<any> | N
+                    ? `${K}?.${PathImpl<Clear<T[K]>, number>}`
+                    : K | `${K}?.${PathImpl<Clear<T[K]>, keyof Clear<T[K]>>}` // JSON?.
+                : K // end PathImpl
+    : never;
 
 export type NonNullableNested<T> = {
 	[P in keyof T]-?: Prettify<
@@ -57,12 +70,19 @@ export type NonNullableNested<T> = {
 export type PathValue<
 	T,
 	P extends Path<T>
-> = P extends `${infer Key}.${infer Rest}`
+> = P extends `${infer NlC}.${infer Rest}`
+	? NlC extends `${infer Key}?`
+		? // FIXME: might be too recursive
+		  // @ts-expect-error
+		  PathValue<Clear<T[Key]>, Rest>
+		: // @ts-expect-error
+		  PathValue<T[NlC], Rest>
+	: P extends `${infer Key}.${infer Rest}`
 	? Key extends keyof T
 		? Rest extends Path<T[Key]>
 			? PathValue<T[Key], Rest>
 			: T[Key] extends ArrayLike<any>
-			? PathValue<T[Key], number>
+			? PathValue<T[Key], Key & number>
 			: never
 		: // @ts-expect-error
 		  PathValue<T[Key], Rest>
